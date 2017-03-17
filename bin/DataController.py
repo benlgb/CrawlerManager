5a817e2ecfc6079f8dedbf1d51036f41c5883af8
Ben <895488201@qq.com>
2017-03-17 12:18:47 +0800

    twitter crawler

--- bin/DataController.py
+++ bin/DataController.py
@@ -10,114 +10,86 @@ import logging
 from urlparse import urljoin
 from src.Request import Request
 from HTMLParser import HTMLParser
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 from src.DataController import DataController
 
 class DataController(DataController):
-	def start(self, input_queue):
-		classifications = ['news', 'business', 'tech', 'culture', 'opinion']
-		for classification in classifications:
+	def start (self, input_queue):
+		with open('1.json') as f:
+			keywords = json.load(f)
+		for keyword in keywords:
 			input_queue.put_nowait(Request(
-				url = 'http://www.ibtimes.com.cn/archives/articles/categories/%s/' % classification,
-				success = self.news_list,
-				data = {
-					'classification': classification,
-					'source_id': 10,
-					'language': 'chi'
+				url = 'https://twitter.com/i/search/timeline',
+				success = self.twitters,
+				keyword = keyword,
+				args = {
+					'params': {
+						'vertical': 'default',
+						'q': keyword,
+						'src': 'typd',
+						'max_position': '',
+						'reset_error_state': False
+					}
 				}
 			))
-	
-	def news_list(self, response, input_queue):
-		logging.info('[+] get news list: %s' % response.url)
-		soup = BeautifulSoup(response.text)
-		for news in soup('li', 'list-style'):
-			data = response.data.copy()
-			img = news.find('img')
-			data.update({
-				'request_url': urljoin(response.url, news.find('a').get('href')),
-				'title': news.find('div', 'title').text,
-				'pub_time': news.find('div', 'timedate').text,
-				'abstract': news.find('div', 'summary').text,
-				'images': [img.get('src')] if img else []
+
+	def twitters(self, response, input_queue):
+		index = 0
+		data = response.json()
+		soup = BeautifulSoup(data['items_html'], 'lxml')
+		ids = response.ids = response.get('ids', [])
+		twitters = response.twitters = response.get('twitters', [])
+		for item in soup('li', 'stream-item'):
+			index += 1
+			tweet = item.find('div', 'tweet')
+			content = tweet.find('div', 'js-tweet-text-container')
+			footer = tweet.find('div', 'ProfileTweet-actionCountList')
+			media = tweet.find('div', 'AdaptiveMediaOuterContainer')
+			if tweet.get('data-tweet-id') in ids:
+				print '[-] find repeat id'
+				continue
+			twitters.append({
+				'id': tweet.get('data-tweet-id'),
+				'content': self.content_fixed(content),
+				'publish_time': tweet.find('span', '_timestamp').get('data-time'),
+				'reply_count': int(footer.find('span', 'ProfileTweet-action--reply').find(
+					'span', 'ProfileTweet-actionCount').get('data-tweet-stat-count')),
+				'retweet_count': int(footer.find('span', 'ProfileTweet-action--retweet').find(
+					'span', 'ProfileTweet-actionCount').get('data-tweet-stat-count')),
+				'favorite_count': int(footer.find('span', 'ProfileTweet-action--favorite').find(
+					'span', 'ProfileTweet-actionCount').get('data-tweet-stat-count')),
+				'ilustris': [img.get('src') for img in media('img')] if media else [],
+				'author': {
+					'id': tweet.get('data-user-id'),
+					'nickname': tweet.get('data-name'),
+					'username': tweet.get('data-screen-name'),
+					'avatar': tweet.find('img').get('src')
+				},
 			})
-			input_queue.put_nowait(Request(
-				url = data['request_url'],
-				success = self.news,
-				data = data
-			))
-		next_page = soup.find('li', 'pager-next')
-		if next_page:
-			next_page = next_page.find('a').get('href')
-			response.url = urljoin(response.url, next_page)
+			ids.append(tweet.get('data-tweet-id'))
+		print ('[+] get %s twitters: %d' % (response.keyword, len(twitters))).encode('gbk')
+		if index == 0:
+			response.zero_times = response.get('zero_times', 0) + 1
+		else:
+			response.zero_times = 0
+		if len(twitters) >= 1000 or response.zero_times > 5:
+			self.save_twitter(twitters, response.keyword)
+		else:
+			response.args['params']['max_position'] = data['min_position']
+			gevent.sleep(1)
 			input_queue.put_nowait(response)
-		
-	def news(self, response, input_queue):
-		logging.info('[+] get news: %s' % response.url)
-		soup = BeautifulSoup(response.text)
-		response.data.update({
-			'response_url': response.response.url,
-			'title': soup.find('div', 'title-header').find('div', 'title').text,
-			'pub_time': soup.find('p', 'timestamp').text,
-			'body': soup('div', 'enlarge-font'),
-			'cole_time': time.time(),
-			'out_links': soup('a')
-		})
-		self.save(response.data)
 
-	def save(self, data):
-		self.count = getattr(self, 'count', 0) + 1
-		data = Statictis(data).filter()
-		data.news_id = self.count
-		path = 'data/%d.json' % self.count
-		with open(path, 'w+') as f:
-			json.dump(data, f, indent=4)
-		logging.info('[+] save news: %d' % self.count)
+	def save_twitter(self, twitters, keyword):
+		print '[+] save twitters'
+		with open(u'data/%s.json' % keyword, 'w+') as f:
+			json.dump({
+				'count': len(twitters),
+				'keyword': keyword,
+				'twitters': twitters
+			}, f, indent=4)
 
-class Statictis(object):
-	def __init__(self, data):
-		self.data = data
-
-	def filter(self):
-		self.pub_time()
-		self.out_links()
-		self.body()
-		self.abstract()
-		self.title()
-		return self.data
-
-	def out_links(self):
-		out_links = []
-		for link in self.data['out_links']:
-			link = link.get('href')
-			if link is not None:
-				out_links.append(link)
-		self.data['out_links'] = out_links
-
-	def body(self):
-		body = '\n'.join([str(body) for body in self.data['body']])
-		self.data['body'] = self.text_handle(body)
-
-	def abstract(self):
-		self.data['abstract'] = self.text_handle(self.data['abstract'])
-
-	def title(self):
-		self.data['title'] = self.text_handle(self.data['title'])
-
-	def pub_time(self):
-		matches = re.search(r'(\d{2,4}).(\d{1,2}).(\d{1,2}).*?(\d{1,2}):(\d{1,2})', self.data['pub_time'])
-		self.data['pub_time'] = '%s-%s-%s %s:%s:00' % matches.groups()
-
-	def text_handle(self, string):
-		string = re.sub(r'<script( .*?|)>[\w\W]*?</script>', '', string)
-		string = re.sub(r'<\/?(p|div|li|ul)( .*?|)>', '\n', string)
-		string = re.sub(r'<!--[\w\W]*?-->|<.*?>', '', string)
-		string = re.sub(r'\s*\n\s*', '\n', string).strip()
-		return HTMLParser().unescape(string)
-
-
-		
-
-
-
-
-
+	def content_fixed(self, content):
+		a = content.find('a')
+		for a in content('a'):
+			a.string = ' %s' % a.text
+		return content.getText()
